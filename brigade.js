913c3190f229dc50ec4b69b1f41fdaf071f16acb
Justin Davies <juda@microsoft.com>
2018-05-17 20:08:38 +0100

    Fix up

--- brigade.js
+++ brigade.js
@@ -14,7 +14,6 @@ events.on("push", (brigadeEvent, project) => {
     const gitSHA = brigadeEvent.revision.commit.substr(0,7)
     const imageTag = String(gitSHA)
 
- 
     // The good stuff
     const frontend = new Job("job-runner-frontend")
     frontend.storage.enabled = false
@@ -33,7 +32,27 @@ events.on("push", (brigadeEvent, project) => {
     ]
 
 
-    Group.runEach([frontend, frontend_helm])
+
+        // The good stuff
+        const api = new Job("job-runner-api")
+        frontend.storage.enabled = false
+        frontend.image = "microsoft/azure-cli"
+        frontend.tasks = [
+            `cd /src/api`,
+            `az login --service-principal -u ${azServicePrincipal} -p ${azClientSecret} --tenant ${azTenant}`,
+            `az acr build -t api:${imageTag}  -f ./Dockerfile . -r ${acrName}`
+        ]
+    
+        const api_helm = new Job("job-runner-api-helm")
+        api_helm.storage.enabled = false
+        api_helm.image = "lachlanevenson/k8s-helm:v2.8.2"
+        api_helm.tasks = [
+            `helm upgrade --install --reuse-values api ./src/Charts/api --set image=${acrServer}/api --set imageTag=${imageTag}`
+        ]
+        Group.runEach([frontend, frontend_helm])
+        Group.runEach([api, api_helm])
+
+
 
 })
 
